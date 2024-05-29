99b4453a4123ee403ab2f464ff9705b285e2f50c
Hyrul <xavier.leonard@outlook.com>
2024-05-29 17:31:23 +0200

    added signup/login

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const bookRoutes = require('./routes/books')
+const userRoutes = require('./routes/user')
+
 const app = express()
 
 mongoose.connect('mongodb+srv://xavierleonard:eoXSiCttSSzgYiWv@cluster0.tkrluzl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -20,5 +22,6 @@ app.use((req, res, next) => {
 
 // Routes de tout ce qui est books (renvoie vers les routes dans le folder routes en vrai)
 app.use('/api/books', bookRoutes)
+app.use('/api/auth', userRoutes)
 
 module.exports = app
---
+++ controllers/user.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+exports.signup = (req, res) => {
+  // Hashing
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      // On prépare l'user (basé sur le model User)
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      })
+      // On l'envoie à la db
+      user.save()
+        .then(() => res.status(201).json({message: 'Utilisateur créé avec succès'}))
+        .catch((err) => res.status(500).json({err}))
+    })
+    .catch((err) => res.status(500).json({err}))
+}
+
+exports.login = (req, res) => {
+  User.findOne({email: req.body.email})
+    .then(user =>{
+      // Check si y a pas d'user
+      if (user === null) {
+        res.status(401).json({message: 'Combo identifiant/mot de passe invalide.'})
+      } else {
+        bcrypt.compare(req.body.password, user.password)
+          // Hash pw comparé avec bcrypt
+          .then(valid => {
+            if (!valid) { res.status(401).json({message: 'Combo identifiant/mot de passe invalide.'})
+            } else {
+              res.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                  // Arguments: ID, clé d'encodage, expiration
+                  {userId: user._id},
+                  'YLnk2s3QXurMsK!*#a', // clé d'encodage
+                  {expiresIn: '72h'}
+                )
+              })} 
+          })
+          .catch((err) => res.status(500).json({err}))
+      }
+    })
+    .catch((err) => res.status(500).json({err}))
+}
